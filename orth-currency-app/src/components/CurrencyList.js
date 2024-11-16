890f1c11ab81800d7909e331c70cc1125e37d215
edogola4 <edogola4@gmail.com>
2024-11-16 12:50:10 +0300

    inital implementation

--- orth-currency-app/src/components/CurrencyList.js
+++ orth-currency-app/src/components/CurrencyList.js
@@ -1,14 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import CurrencyItem from "./CurrencyItem";
 
 const ListWrapper = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+const EmptyMessage = styled.div`
+  padding: 1rem;
+  text-align: center;
+  color: #555;
+  font-size: 1rem;
 `;
 
 const CurrencyList = ({ currencies }) => {
+  if (!currencies || currencies.length === 0) {
+    return <EmptyMessage>No currencies available to display.</EmptyMessage>;
+  }
+
   return (
     <ListWrapper>
       {currencies.map((currency) => (
@@ -16,6 +31,22 @@ const CurrencyList = ({ currencies }) => {
       ))}
     </ListWrapper>
   );
+};
+
+// Prop validation
+CurrencyList.propTypes = {
+  currencies: PropTypes.arrayOf(
+    PropTypes.shape({
+      code: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      rate: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+CurrencyList.defaultProps = {
+  currencies: [],
 };
 
 export default CurrencyList;
